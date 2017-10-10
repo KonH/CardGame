ebe2338922ef80d3c1fd702ebbbb601d7c1250aa
KonH <konh@yandex.ru>
2017-10-10 20:49:53 +0700

    UDBase import & init;

---
+++ UnityClient/Assets/UDBase_Project/Schemes/DevScheme.cs
@@ -0,0 +1,12 @@
+#if Scheme_DevScheme
+using UnityEngine;
+using System.Collections;
+using UDBase.Common;
+
+public class ProjectScheme : Scheme {
+
+	public ProjectScheme() {
+		// Add your initialization code here
+	}
+}
+#endif
