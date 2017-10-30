b2b4de3a3d33189e0f21a0f0c0710d682df85816
KonH <konh@yandex.ru>
2017-10-30 23:56:23 +0700

    Server: Test game controller with state changes;

--- Server/Controllers/GameController.cs
+++ Server/Controllers/GameController.cs
@@ -1,24 +1,70 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using SharedLibrary.Common;
 using SharedLibrary.Models;
 using SharedLibrary.Models.Game;
+using SharedLibrary.Actions;
+using SharedLibrary.Utils;
 
 namespace Server.Controllers {
 	[Authorize]
 	[Produces("application/json")]
 	[Route("api/game")]
 	public class GameController : Controller {
-		GameState _state = new GameState() {
-			Users = new List<UserState> {
-				new UserState() { Name = "user1", Health = 11, MaxHealth = 20},
-				new UserState() { Name = "user2", Health = 12, MaxHealth = 20},
+		static GameState _state = new GameState("", new List<UserState> {
+				new UserState("user1", 10),
+				new UserState("user2", 10)
 			}
-		};
+		);
+		static List<IGameAction> _actions = new List<IGameAction>();
 
-		[HttpGet]
+		ILogger _logger;
+
+		public GameController(ILoggerFactory loggerFactory) {
+			_logger = loggerFactory.CreateLogger(typeof(GameController));
+		}
+
+		[HttpGet("state")]
 		public IActionResult GetGameState() {
+			// TODO: Filter user state
 			return Json(_state);
+		}
+
+		IGameAction GetActionForVersion(int version) {
+			if ( _actions.Count > version ) {
+				return _actions[version];
+			}
+			return null;
+		}
+
+		[HttpGet("action")]
+		public IActionResult GetAction(int version) {
+			var action = GetActionForVersion(version);
+			if ( action != null ) {
+				Response.HttpContext.Response.Headers.Add(Interaction.ActionTypeHeader, action.GetType().ToString());
+				return Json(action);
+			}
+			return new EmptyResult();
+		}
+
+		[HttpPost("action")]
+		public IActionResult PostAction(int version, string type, [FromBody] JObject action) {
+			var typeInstance = ReflectionUtils.GetActionType(type);
+			if ( typeInstance != null ) {
+				var actionInstance = action.ToObject(typeInstance) as IGameAction;
+				if ( actionInstance != null ) {
+					lock ( _state ) {
+						if ( _state.TryApply(actionInstance) ) {
+							_actions.Add(actionInstance);
+							return Ok();
+						}
+					}
+				}
+			}
+			return BadRequest();
 		}
 	}
 }
