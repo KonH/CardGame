2352c7093b9d02066cc15c3566879d978d1b81e4
KonH <konh@yandex.ru>
2017-11-04 13:41:17 +0700

    Client: Initial setup for cards, sub-module changed;

---
+++ UnityClient/Assets/Scripts/UI/CardView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour {
+	public Text Name;
+	public Text Price;
+	public Text Attack;
+	public Text Hp;
+}
--- UnityClient/Assets/Scripts/UI/UserView.cs
+++ UnityClient/Assets/Scripts/UI/UserView.cs
@@ -4,4 +4,7 @@ using UnityEngine.UI;
 public class UserView : MonoBehaviour {
 	public Text NameText;
 	public Text HPText;
+	public CardSet Global;
+	public CardSet Hand;
+	public CardSet Table;
 }
