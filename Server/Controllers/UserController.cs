735a303a2c59eb0b412cb26cd2768a15f4a83165
KonH <konh@yandex.ru>
2017-11-27 21:45:53 +0700

    Server: Read-only user controller with admin access;

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Server.Repositories;
+
+namespace Server.Controllers {
+	[Authorize(Roles = "admin")]
+	[Produces("application/json")]
+	[Route("api/user")]
+	public class UserController : Controller {
+		ILogger         _logger;
+		IUserRepository _users;
+
+		public UserController(ILogger<UserController> logger, IUserRepository users) {
+			_logger = logger;
+			_users  = users;
+		}
+
+		[HttpGet]
+		public IActionResult Index() {
+			return Json(_users.All);
+		}
+	}
+}
